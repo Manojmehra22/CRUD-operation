c515728cde90b4b4c6ec841b56b6e792a495736a
Manojmehra22 <manojmehr650@gmail.com>
2024-11-09 18:22:02 +0530

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,190 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { EmployeeData } from "./EmployeeData";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [age, setAge] = useState(0);
+  const [id, setId] = useState(0);
+  const [isUpdate, setIsUpdate] = useState(false);
+
+  useEffect(() => {
+    setData(EmployeeData);
+  }, []);
+
+  const handleEdit = (id) => {
+    const dt = data.filter((item) => item.id === id);
+    if (dt !== undefined) {
+      setIsUpdate(true);
+      setId(id);
+      setFirstName(dt[0].firstName);
+      setLastName(dt[0].lastName);
+      setAge(dt[0].age);
+    }
+  };
+
+  const handleDelete = (id) => {
+    if (id > 0) {
+      if (window.confirm("Are you sure to delete this Row?")) {
+        const dt = data.filter((item) => item.id !== id);
+        setData(dt);
+      }
+    }
+  };
+
+  const handleSave = (e) => {
+    let error = "";
+
+    if (firstName === "") error += "FirstName is required,";
+
+    if (lastName === "") error += "lastname  is required,";
+
+    if (age <= 0) error += "Age is required.";
+
+    if (error === "") {
+      e.preventDefault();
+      const dt = [...data];
+      const newObject = {
+        id: EmployeeData.length + 1,
+        firstName: firstName,
+        lastName: lastName,
+        age: age,
+      };
+      dt.push(newObject);
+      setData(dt);
+    } else {
+      alert(error);
+    }
+  };
+  const handleUpdate = () => {
+    const index = data
+      .map((item) => {
+        return item.id;
+      })
+      .indexOf(id);
+    const dt = [...data];
+    dt[index].firstName = firstName;
+    dt[index].lastName = lastName;
+    dt[index].age = age;
+
+    setData(dt);
+    handleClear();
+  };
+  const handleClear = () => {
+    setId(0);
+    setFirstName("");
+    setLastName("");
+    setAge("");
+    setIsUpdate(false);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "3rem",
+          marginBottom: "10px",
+          gap: "3rem",
+        }}
+      >
+        <div>
+          <label>
+            First Name:
+            <input
+              type="text"
+              placeholder="Enter First name"
+              onChange={(e) => setFirstName(e.target.value)}
+              value={firstName}
+              className="input"
+            ></input>
+          </label>
+        </div>
+        <div>
+          <label>
+            Last Name:
+            <input
+              type="text"
+              placeholder="Enter Last name"
+              onChange={(e) => setLastName(e.target.value)}
+              value={lastName}
+              className="input"
+            ></input>
+          </label>
+        </div>
+        <div>
+          <label>
+            Age:
+            <input
+              type="text"
+              placeholder="Enter your age"
+              onChange={(e) => setAge(e.target.value)}
+              value={age}
+              className="input"
+            ></input>
+          </label>
+        </div>
+        <div className="saveclear">
+          {!isUpdate ? (
+            <button className="btn btn-primary" onClick={(e) => handleSave(e)}>
+              Save
+            </button>
+          ) : (
+            <button className="btn btn-primary" onClick={() => handleUpdate()}>
+              Update
+            </button>
+          )}
+
+          <button className="btn btn-danger" onClick={() => handleClear()}>
+            Clear
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <table className="table table-hover">
+        <thead>
+          <tr className="heading">
+            <td>Sr.No.</td>
+            <td>Id</td>
+            <td>First Name</td>
+            <td>Last Name</td>
+            <td>Age</td>
+            <td style={{ paddingLeft: "2.2rem" }}>Actions</td>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => {
+            return (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{item.id}</td>
+                <td>{item.firstName}</td>
+                <td>{item.lastName}</td>
+                <td>{item.age}</td>
+                <td className="btns">
+                  <button
+                    className=" edit btn btn-primary"
+                    onClick={() => handleEdit(item.id)}
+                  >
+                    Edit
+                  </button>
+
+                  <button
+                    className=" delete btn btn-danger"
+                    onClick={() => handleDelete(item.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/EmployeeData.jsx
@@ -0,0 +1,20 @@
+export const EmployeeData = [
+  {
+    id: 1,
+    firstName: "henry",
+    lastName: "klasen",
+    age: "45",
+  },
+  {
+    id: 2,
+    firstName: "quinton",
+    lastName: "decock",
+    age: "18",
+  },
+  {
+    id: 3,
+    firstName: "crugor",
+    lastName: "reacton",
+    age: "89",
+  },
+];
